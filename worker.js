181c7bc01a5814edb9eb41427ffa9de67991910f
にゃふさんあああ <117125474+nyahunyahudau@users.noreply.github.com>
2024-08-20 06:46:43 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,32 +1,25 @@
-onmessage = function(event) {
-    const { url, rps } = event.data;
-    let successCount = 0;
-    let errorCount = 0;
+self.onmessage = function(event) {
+    const { rps } = event.data;
+    const interval = 1000 / rps;
 
-    const sendRequest = () => {
-        fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    successCount++;
-                } else {
-                    errorCount++;
-                    console.error(`Request failed with status: ${response.status}`);
+    let requestsCompleted = 0;
+
+    function makeRequest() {
+        // Example of a request
+        fetch('https://example.com/api')
+            .then(() => {
+                requestsCompleted++;
+                if (requestsCompleted % 100 === 0) {
+                    postMessage({ requestsCompleted });
                 }
             })
-            .catch(error => {
-                errorCount++;
-                console.error(`Request error: ${error.message}`);
+            .catch(() => {
+                // Handle error
+            })
+            .finally(() => {
+                setTimeout(makeRequest, interval);
             });
-    };
+    }
 
-    const intervalId = setInterval(() => {
-        for (let i = 0; i < rps; i++) {
-            sendRequest();
-        }
-    }, 1000);
-
-    setTimeout(() => {
-        clearInterval(intervalId);
-        postMessage({ successCount, errorCount });
-    }, 5000); // Run the test for 5 seconds
+    makeRequest();
 };
